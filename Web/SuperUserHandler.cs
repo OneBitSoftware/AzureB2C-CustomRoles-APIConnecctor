1a7e3ccfbbbee9b34e60256c49f9315f2d36f088
Radi Atanassov <radi@sharepoint.bg>
2021-11-30 10:22:54 +0100

    Commiitting the web project

---
+++ Web/SuperUserHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace ECS2021
+{
+    public class SuperUserHandler : AuthorizationHandler<SuperUserRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, SuperUserRequirement requirement)
+        {
+            var superUserClaim = context.User.FindFirst(c => c.Type == "extension_role");
+
+            if (superUserClaim != null && superUserClaim.Value.Equals(requirement.SuperUserValue))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
